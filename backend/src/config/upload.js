d3f2f61b520bc030fe59350e589351ada37d344f
sinapad <malinoski.iuri@gmail.com>
2019-10-03 09:22:47 -0300

    image name bug fix

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -2,9 +2,17 @@ const multer = require('multer');
 const path = require('path')
 module.exports = {
     storage: multer.diskStorage({
+
         destination: path.resolve(__dirname, '..', '..', 'uploads'), // '..', '..' garantee the relative path for linux (/) or windows (\)
+        
+        // 'filename' is a variable where receive an function (requisition, file received, and callback where return a response when finishes)
         filename: (req, file, cb) => {
-            cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+            
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext);
+
+            // callback function (if occour an error, name of file (using Template Strings ${} ) )
+            cb(null, `${name}-${Date.now()}${ext}`)
         },
     })
 }
