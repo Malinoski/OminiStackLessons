108d0acddb94412a059d8ce0c1f4df82df9703e8
sinapad <malinoski.iuri@gmail.com>
2019-10-15 15:49:31 -0300

    added first css layout on the form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 /*
 function App() {
@@ -11,7 +12,27 @@ function App() {
 
 function App() {
   return (
-    <h1></h1>
+    <div className="container">
+
+      {/* {someName} is javascript */}
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>
+          Offer <strong>spots</strong> to programmers and find talented souls for you company
+        </p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email"
+            id="email"
+            placeholder="put your email here"
+          />
+          <button type="submit" class="btn">Enter</button>
+        </form>
+      </div>
+
+    </div>
   );
 }
 
